11d0199bd99a2f99e5ac2b8ce94cda68c261d3cf
stephane <stephane@22decembre.eu>
2014-02-02 16:13:00 +0100

    add logging support in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,10 @@ from flask.ext.login import LoginManager, login_user, UserMixin, login_required,
 import os
 from flask.ext.sqlalchemy import SQLAlchemy
 
+
+
+
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
@@ -17,4 +21,12 @@ from app import views
 
 from moment import momentjs
 app.jinja_env.globals['momentjs'] = momentjs
-#app.jinja_env.filters['filesize'] = filesize.do_filesize
+
+import logging
+from logging.handlers import RotatingFileHandler
+file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+app.logger.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.info('microblog startup')
