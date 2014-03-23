17d0b0b490d80237460a8719a3942ab61bdf78f6
stephane <stephane@22decembre.eu>
2014-03-23 17:58:49 +0100

    continue setup for finish function

--- app/__init__.py
+++ app/__init__.py
@@ -20,9 +20,9 @@ app.jinja_env.globals['momentjs'] = momentjs
 
 import logging
 from logging.handlers import RotatingFileHandler
-file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+file_handler = RotatingFileHandler('tmp/torrent.log', 'a', 1 * 1024 * 1024, 10)
 file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
 app.logger.setLevel(logging.INFO)
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
-app.logger.info('microblog startup')
+app.logger.info('Strom startup')
--- app/finish.py
+++ app/finish.py
@@ -12,10 +12,15 @@ import sys, os, base64
 mail = Mail(app)
 
 torrent_fini = client.get_torrent(TR_TORRENT_HASH)
-msg = Message('torrent fini', sender = 'torrents@22decembre.eu', recipients = 'stephane@22decembre.eu')
+query = Torrent.query.filter_by(hashString = unicode(torrent_fini)).all()
+user = User(ident=query.user)
+
+msg = Message('torrent fini', sender = 'torrents@22decembre.eu', recipients = user.mail)
 msg.body = torrent_fini.name
 #msg.html = '<b>HTML</b> body'
 #with app.app_context():
 mail.send(msg)
+
+app.logger.info('%(torrent_fini)% de %(user)% terminé.')
 # mail
 # déplacer les fichiers dans le dossier de l'utilisateur
