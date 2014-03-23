7ff3ffe1c9e3b37e1553028ce26bda5d54d4a9df
stephane <stephane@22decembre.eu>
2014-03-23 16:53:42 +0100

    begin to setup 'script at the end of download' function need to be installed on the local download server (same host as transmission daemon)

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect, session, url_for, request, g, json
+from flask import render_template, flash, redirect, session, url_for, request, g, json, jsonify
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from werkzeug import secure_filename
 from app   import app, db, lm
 from config import basedir, ADMINS
-from forms import TorrentSeedForm, TorrentFileDetails, TorrentForm, LoginForm
+from forms import TorrentSeedForm, TorrentFileDetails, TorrentForm, LoginForm, Torrents, TorrentBandwidth
 from models import User, Torrent
 import transmissionrpc as tr
 import sys, os, base64, magic
@@ -17,7 +17,8 @@ client = tr.Client(address=app.config['TRANSMISSION_HOST'],
 # tr_session car il y a déjà un session quelque part : la session web avec auth et mots de passe (entre autre)
 tr_session = tr.Session(client)
 tr_session.script_torrent_done_enabled = True
-tr_session.script_torrent_done_filename = "finish.py"
+tr_session.script_torrent_done_filename = os.path.dirname(os.path.realpath(__file__))."/finish.py"
+
 
 def redirect_url():
     return request.referrer or url_for('index')
@@ -101,11 +102,11 @@ def torrent(tor_id):
 		#	torrent.seedRatioMode = 'Individual ratio limit'
 		#if torrent.seedRatioMode == 2:
 		#	torrent.seedRatioMode = 'Unlimited seeding'
-		files_number = 0
+		#files_number = 0
 		control = TorrentForm(ratiomode=torrent.seedRatioMode,bandwidthpriority=torrent.bandwidthPriority)
 		###
 		for file_x in client.get_files(tor_id)[torrent.id]:
-			print(client.get_files(tor_id)[torrent.id][file_x])
+			#print(client.get_files(tor_id)[torrent.id][file_x])
 			# no csrf because this form is just a part of a bigger one which has already its own csrf !
 			#f_form = TorrentFileDetails(csrf_enabled=False)
 			f_form = TorrentFileDetails()
@@ -199,14 +200,18 @@ def torrent(tor_id):
 			print(control.ratiomode.data)
 		return render_template("torrent.html", title = torrent.name, user = user, torrent = torrent, control = control)
 
+@app.route('/updatebandwidthpriority/<tor_id>/<new_prio>')
+@login_required
+def updatebandwidthpriority(tor_id,new_prio):
+	torrent = client.get_torrent(tor_id)
+	torrent.priority = new_prio
+	torrent.update()
+
 @app.route('/start/<tor_id>', methods = ['GET','POST'])
 @login_required
 def start(tor_id):
 	torrent = client.get_torrent(tor_id)
-	if action=='start':
-		torrent.start()
-	if action=='stop':
-		torrent.stop()
+	torrent.start()
 	return redirect(redirect_url())
 
 @app.route('/stop/<tor_id>', methods = ['GET','POST'])
@@ -245,8 +250,30 @@ def index():
 	listing =list()
 	for x in torrents_from_db:
 		listing.append(x.hashstring)
-	torrents = client.get_torrents(listing)
 	
+	torrents_forms = dict()
+	torrents = client.get_torrents(listing)
+	for torrent in torrents:
+		form = TorrentBandwidth()
+		form.tor_id = torrent.hashString
+		#form.bandwidthpriority = torrent.bandwidthPriority
+		torrents_forms[torrent.hashString]=form
+
+	#control_form = Torrents()
+	###
+	#for torrent_x in torrents:
+		#t_form = TorrentBandwidth(csrf_enabled=False)
+		#t_form = torrent_x
+		#t_form.bandwidthpriority = torrent_x.bandwidthPriority
+		#t_form.tor_progress = torrent_x.progress
+		#t_form.tor_id=torrent_x.hashString
+		#t_form.tor_status=torrent_x.status
+		#t_form.tor_name=torrent_x.name
+		#control_form.torrents.append_entry(t_form)
+	
+	#if control.validate_on_submit():
+		
+		
 	# envoi d'un nouveau torrent
 	form = TorrentSeedForm()
 	if form.validate_on_submit():
@@ -260,11 +287,12 @@ def index():
 		try:
 			# ON ajoute le torrent à transmission
 			new_tor = client.add_torrent(torrent_to_start)
-			new_tor.downloadDir = g.user.dl_dir
+			new_tor.start()
+			# new_tor.downloadDir = g.user.dl_dir
 			
-			#app.logger.info('%(new_tor)% demarré et ajouté à la base de données par %(user)%.')
+			# app.logger.info('%(new_tor)% demarré et ajouté à la base de données par %(user)%.')
 			
-			new_tor.update()
+			#new_tor.update()
 			
 			# on ajoute le torrent à la base de données pour se souvenir à qui il appartient.
 			torrent_to_add = Torrent(hashstring=new_tor.hashString,user=unicode(g.user))
@@ -275,7 +303,7 @@ def index():
 			app.logger.info(tr.TransmissionError)
 		return redirect(redirect_url())
 		
-	return render_template("index.html", form = form, title = "Home", user = g.user, torrents = torrents)
+	return render_template("index.html", form = form, title = "Home", user = g.user, torrents = torrents, torrents_forms = torrents_forms)
 
 @app.route('/admin', methods = ['GET', 'POST'])
 @login_required
